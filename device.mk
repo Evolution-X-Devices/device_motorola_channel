80db9ebcaf564c2de5f071024c81d07110862152
Deivid21 <david.parra.ignacio@gmail.com>
2025-07-30 03:47:29 -0400

    channel: Move thermal-engine to vendor
    Change-Id: I21e484c0acf2830e42947f2cb429bb4fcae23f01

--- device.mk
+++ device.mk
@@ -80,10 +80,5 @@ PRODUCT_COPY_FILES += \
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 28
 
-# Thermal
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine-channel-NA_TMO.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-channel-NA_TMO.conf \
-    $(LOCAL_PATH)/configs/thermal-engine-channel.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-channel.conf
-
 # Inherit from motorola sdm632-common
 $(call inherit-product, device/motorola/sdm632-common/common.mk)
